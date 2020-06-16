c01d5bd8ae627687cb136969c0643adff607b653
MRegirouard <46038297+MRegirouard@users.noreply.github.com>
2020-06-16 11:35:37 -0400

    Update target.mk

--- src/main/target/CLRACINGF4/target.mk
+++ src/main/target/CLRACINGF4/target.mk
@@ -1,9 +1,9 @@
 F405_TARGETS   += $(TARGET)
 FEATURES       = VCP SDCARD_SPI
 TARGET_SRC = \
-            drivers/accgyro/accgyro_mpu.c \
-            drivers/accgyro/accgyro_mpu6500.c \
-            drivers/accgyro/accgyro_mpu6000.c \
-            drivers/accgyro/accgyro_mpu6500.c \
-            drivers/max7456.c
-            drivers/light_ws2811strip.c
+	drivers/accgyro/accgyro_mpu.c \
+	drivers/accgyro/accgyro_mpu6500.c \
+	drivers/accgyro/accgyro_mpu6000.c \
+	drivers/accgyro/accgyro_mpu6500.c \
+	drivers/max7456.c
+	drivers/light_ws2811strip.c
