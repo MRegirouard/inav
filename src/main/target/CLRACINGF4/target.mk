9a90e05ac3434406340476ff817f5ff90c88ab2e
MRegirouard <46038297+MRegirouard@users.noreply.github.com>
2020-06-16 11:11:24 -0400

    Create target.mk

---
+++ src/main/target/CLRACINGF4/target.mk
@@ -0,0 +1,8 @@
+F405_TARGETS   += $(TARGET)
+FEATURES       = VCP SDCARD_SPI
+TARGET_SRC = \
+            drivers/accgyro/accgyro_mpu.c \
+            drivers/accgyro/accgyro_mpu6500.c \
+            drivers/accgyro/accgyro_mpu6000.c \
+            drivers/accgyro/accgyro_mpu6500.c \
+            drivers/max7456.c
