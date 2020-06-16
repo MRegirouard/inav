7f5fc7d3acc02507f77fed6f40bd90cdd4300643
MRegirouard <46038297+MRegirouard@users.noreply.github.com>
2020-06-16 11:09:24 -0400

    Update target.h

--- src/main/target/CLRACINGF4/target.h
+++ src/main/target/CLRACINGF4/target.h
@@ -28,17 +28,81 @@
 
 #define USBD_PRODUCT_STRING "CLRACINGF4"
 
-#define USE_TARGET_CONFIG
-
 #define LED0     PB5
+
 #define BEEPER   PB4
 #define BEEPER_INVERTED
 
-#define INVERTER_PIN_UART PC0
-
+#define USE_EXTI
+#define GYRO_INT_EXTI            PC4
 #define USE_MPU_DATA_READY_SIGNAL
 
-#define GYRO_INT_EXTI            PC4
-#define USE_EXTI
-
 #define USE_IMU_MPU6000
+#define IMU_MPU6000_ALIGN       CW0_DEG
+#define MPU6000_CS_PIN          PA4
+#define MPU6000_SPI_BUS         BUS_SPI1
+
+#define USE_VCP
+#define VBUS_SENSING_PIN        PC5
+#define VBUS_SENSING_ENABLED
+
+#define USE_UART1
+#define UART1_RX_PIN            PA10
+#define UART1_TX_PIN            PA9
+#define INVERTER_PIN_UART1_RX   PC0
+
+#define USE_UART3
+#define UART3_RX_PIN            PB11
+#define UART3_TX_PIN            PB10
+
+#define USE_UART6
+#define UART6_RX_PIN            PC7
+#define UART6_TX_PIN            PC6
+
+#define USE_UART4
+#define UART4_RX_PIN            PA1
+#define UART4_TX_PIN            PA0
+
+#define USE_SOFTSERIAL1
+// #define SOFTSERIAL_1_RX_PIN     PC8
+// #define SOFTSERIAL_1_TX_PIN     PC9
+
+#define SERIAL_PORT_COUNT       6
+
+#define USE_SPI
+
+#define USE_SPI_DEVICE_1
+
+#define USE_SPI_DEVICE_2
+#define SPI2_NSS_PIN            PB12
+#define SPI2_SCK_PIN            PB13
+#define SPI2_MISO_PIN           PB14
+#define SPI2_MOSI_PIN           PB15
+
+#define USE_SPI_DEVICE_3
+#define SPI3_NSS_PIN            PA15
+#define SPI3_SCK_PIN            PC10
+#define SPI3_MISO_PIN           PC11
+#define SPI3_MOSI_PIN           PC12
+
+#define USE_ADC
+#define CURRENT_METER_ADC_PIN   PC1
+#define VBAT_ADC_PIN            PC2
+#define RSSI_ADC_PIN            PA0
+#define DEFAULT_VOLTAGE_METER_SOURCE VOLTAGE_METER_ADC
+#define DEFAULT_CURRENT_METER_SOURCE CURRENT_METER_ADC
+
+#define SENSORS_SET (SENSOR_ACC)
+
+#define USE_LED_STRIP
+#define WS2811_PIN                      PB09
+
+#define DEFAULT_FEATURES        (FEATURE_BLACKBOX | FEATURE_VBAT)
+
+#define MAX_PWM_OUTPUT_PORTS    6
+#define TARGET_MOTOR_COUNT      4
+
+#define TARGET_IO_PORTA         0xffff
+#define TARGET_IO_PORTB         0xffff
+#define TARGET_IO_PORTC         0xffff
+#define TARGET_IO_PORTD         0xffff
